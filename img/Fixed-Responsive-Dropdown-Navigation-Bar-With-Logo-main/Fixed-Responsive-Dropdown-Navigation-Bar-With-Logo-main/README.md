8eb5e52feb502f2af1bdf9fd00a3c28d5ad0244a
Kaleem Ullah Moon <kaleemullah987@gmail.com>
2023-02-28 02:40:14 +0500

    css added

---
+++ img/Fixed-Responsive-Dropdown-Navigation-Bar-With-Logo-main/Fixed-Responsive-Dropdown-Navigation-Bar-With-Logo-main/README.md
@@ -0,0 +1,6 @@
+# Fixed-Responsive-Dropdown-Navigation-Bar-With-Logo
+
+## More Website Development Content Visit [RustcodeWeb](https://www.rustcodeweb.com/)
+
+### Fixed Responsive Dropdown Navigation Bar With Logo | HTML, CSS And JQuery
+[Youtube Video](https://youtu.be/vxX5I17dYos)
--- js/slide.js
+++ js/slide.js
@@ -6,3 +6,25 @@ document.getElementById("next").onclick = function () {
     const widthItem = document.querySelector(".item").offsetWidth;
     document.getElementById("formList").scrollLeft -= widthItem;
   };
+
+
+
+  document.getElementById("for").onclick = function () {
+    const widthItem = document.querySelector(".item").offsetWidth;
+    document.getElementById("form_List").scrollLeft += widthItem;
+  };
+  document.getElementById("pre").onclick = function () {
+    const widthItem = document.querySelector(".item").offsetWidth;
+    document.getElementById("form_List").scrollLeft -= widthItem;
+  };
+
+
+
+  document.getElementById("forw").onclick = function () {
+    const widthItem = document.querySelector(".item").offsetWidth;
+    document.getElementById("client_formList").scrollLeft += widthItem;
+  };
+  document.getElementById("pree").onclick = function () {
+    const widthItem = document.querySelector(".item").offsetWidth;
+    document.getElementById("client_formList").scrollLeft -= widthItem;
+  };
