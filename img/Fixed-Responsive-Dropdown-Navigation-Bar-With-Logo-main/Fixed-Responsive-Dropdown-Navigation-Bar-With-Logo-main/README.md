5f0bb1d34ccfc753d4e79891c6b17cbdead1d255
Kaleem Ullah Moon <kaleemullah987@gmail.com>
2023-02-28 20:39:15 +0500

    css add

--- img/Fixed-Responsive-Dropdown-Navigation-Bar-With-Logo-main/Fixed-Responsive-Dropdown-Navigation-Bar-With-Logo-main/README.md
+++
@@ -1,6 +0,0 @@
-# Fixed-Responsive-Dropdown-Navigation-Bar-With-Logo
-
-## More Website Development Content Visit [RustcodeWeb](https://www.rustcodeweb.com/)
-
-### Fixed Responsive Dropdown Navigation Bar With Logo | HTML, CSS And JQuery
-[Youtube Video](https://youtu.be/vxX5I17dYos)
--- js/slide.js
+++ js/slide.js
@@ -1,30 +1,50 @@
-document.getElementById("next").onclick = function () {
-    const widthItem = document.querySelector(".item").offsetWidth;
-    document.getElementById("formList").scrollLeft += widthItem;
-  };
-  document.getElementById("prev").onclick = function () {
-    const widthItem = document.querySelector(".item").offsetWidth;
-    document.getElementById("formList").scrollLeft -= widthItem;
-  };
+// document.getElementById("next").onclick = function () {
+//     const widthItem = document.querySelector(".item").offsetWidth;
+//     document.getElementById("formList").scrollLeft += widthItem;
+//   };
+//   document.getElementById("prev").onclick = function () {
+//     const widthItem = document.querySelector(".item").offsetWidth;
+//     document.getElementById("formList").scrollLeft -= widthItem;
+//   };
 
+//   document.getElementById("for").onclick = function () {
+//     const widthItem = document.querySelector(".item").offsetWidth;
+//     document.getElementById("form_List").scrollLeft += widthItem;
+//   };
+//   document.getElementById("pre").onclick = function () {
+//     const widthItem = document.querySelector(".item").offsetWidth;
+//     document.getElementById("form_List").scrollLeft -= widthItem;
+//   };
 
+//   document.getElementById("forw").onclick = function () {
+//     const widthItem = document.querySelector(".item").offsetWidth;
+//     document.getElementById("client_formList").scrollLeft += widthItem;
+//   };
+//   document.getElementById("pree").onclick = function () {
+//     const widthItem = document.querySelector(".item").offsetWidth;
+//     document.getElementById("client_formList").scrollLeft -= widthItem;
+//   };
 
-  document.getElementById("for").onclick = function () {
-    const widthItem = document.querySelector(".item").offsetWidth;
-    document.getElementById("form_List").scrollLeft += widthItem;
-  };
-  document.getElementById("pre").onclick = function () {
-    const widthItem = document.querySelector(".item").offsetWidth;
-    document.getElementById("form_List").scrollLeft -= widthItem;
-  };
-
-
-
-  document.getElementById("forw").onclick = function () {
-    const widthItem = document.querySelector(".item").offsetWidth;
-    document.getElementById("client_formList").scrollLeft += widthItem;
-  };
-  document.getElementById("pree").onclick = function () {
-    const widthItem = document.querySelector(".item").offsetWidth;
-    document.getElementById("client_formList").scrollLeft -= widthItem;
-  };
+///////////////////OWL Carosuel///////////////////////////
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    // nav:true,
+    slideBy:2,
+    autoplay:true,
+    animateIn:'fadeIn',
+    animateOut:'fadeOut',
+    loop: true,
+    margin: 10, 
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 5,
+      },
+    },
+  });
+});
