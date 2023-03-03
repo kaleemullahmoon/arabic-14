7053c3247911f5324bfdc6bb288b1810d24874e5
Kaleem Ullah Moon <kaleemullah987@gmail.com>
2023-03-03 15:03:26 +0500

    css add

--- js/nav.js
+++ js/nav.js
@@ -12,8 +12,8 @@ const navSlide = () => {
       if (link.style.animation) {
         link.style.animation = "";
       } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${
-          index / 7 + 0.5
+        link.style.animation = `navLinkFade 0.1s ease forwards ${
+          index / 7 + 0.1
         }s`;
       }
     });
